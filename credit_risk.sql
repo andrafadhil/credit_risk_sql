2e551f33e5e3fe38e0f2f87a416e94e91a907918
fadhil <141062946+andrafadhil@users.noreply.github.com>
2024-09-10 02:46:30 +0700

    Update credit_risk.sql

--- credit_risk.sql
+++ credit_risk.sql
@@ -20,12 +20,12 @@ WHERE person_emp_length > 47
 
 -- What is the performance distribution of the loan portfolio, in terms of repayment and default?
 SELECT 
-	CASE 
-	    WHEN loan_status = '1' THEN 'default'
-	    WHEN loan_status = '0' THEN 'non-default'
-	END AS customer_history,
-	COUNT(*) AS total_loans,
-	ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM credit_risk), 2) AS presentase
+    CASE 
+	WHEN loan_status = '1' THEN 'default'
+	WHEN loan_status = '0' THEN 'non-default'
+    END AS customer_history,
+    COUNT(*) AS total_loans,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM credit_risk), 2) AS presentase
 FROM credit_risk
 GROUP BY customer_history
 ORDER BY total_loans;
@@ -121,12 +121,12 @@ ORDER BY default_rate DESC;
 
 -- How likely is a customer with a history of default to default again?
 SELECT 
-	CASE 
-	    WHEN cb_person_default_on_file = 'Y' THEN 'default'
-	    WHEN cb_person_default_on_file = 'N' THEN 'non-default'
-	END AS customer_history,
-	COUNT(*) AS total_loans,
-	ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM credit_risk), 2) AS presentase
+    CASE 
+	WHEN cb_person_default_on_file = 'Y' THEN 'default'
+	WHEN cb_person_default_on_file = 'N' THEN 'non-default'
+    END AS customer_history,
+    COUNT(*) AS total_loans,
+    ROUND(COUNT(*) * 100.0 / (SELECT COUNT(*) FROM credit_risk), 2) AS presentase
 FROM credit_risk
 GROUP BY customer_history
 ORDER BY total_loans DESC;
